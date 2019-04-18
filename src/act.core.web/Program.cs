26fe711364cadeb566a3457142fe573fdc44bb79
Jay Brummels <jay.brummels@csgi.com>
2019-04-18 09:23:26 -0500

    removing customer HTTPS Kestrel Setup that carried over from netcore mvc 1.* in favor of built in Kestrel configuration.

--- src/act.core.web/Program.cs
+++ src/act.core.web/Program.cs
@@ -12,31 +12,10 @@ namespace act.core.web
         {
             BuildWebHost(args).Run();
         }
-        private static Dictionary<string, string> ParseEbConfig(IConfiguration config)
-        {
-            Dictionary<string, string> dict = new Dictionary<string, string>();
-
-            foreach (IConfigurationSection pair in config.GetSection("iis:env").GetChildren())
-            {
-                string[] keypair = pair.Value.Split(new[] { '=' }, 2);
-                dict.Add(keypair[0], keypair[1]);
-            }
-
-            return dict;
-        }
+       
         private static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                {
-                    if (!hostingContext.HostingEnvironment.IsDevelopment())
-                    {
-                        config.AddJsonFile(@"C:\Program Files\Amazon\ElasticBeanstalk\config\containerconfiguration", true, true);
-                        config.AddInMemoryCollection(ParseEbConfig(config.Build()));
-                    }
-                })
                 .UseStartup<Startup>()
-                .UseKestrel(o => o.ConfigureEndpoints())
-                .UseIISIntegration()
                 .Build();
 
   
