600369b7fab8f5e14770010cbfec517ef4712b74
Sayanta Basu <sayanta.personal@gmail.com>
2020-06-22 23:16:41 -0600

    Patch 1 (#37)
    * updated lambda csproj
    
    * Added cert file into act.core.etl.csproj
    
    * Reverting changes related self signed certificate support

--- src/act.core.etl/Gatherer.cs
+++ src/act.core.etl/Gatherer.cs
@@ -283,12 +283,8 @@ namespace act.core.etl
         }
 
         private async Task<T> PostRequest<T>(int environmentId, string url, string json = null)
-        {
-            var handler = new HttpClientHandler();
-            handler.ClientCertificateOptions = ClientCertificateOption.Manual;
-            handler.ServerCertificateCustomValidationCallback = ValidateServerCertificate;
-                ;
-            using (var client = new HttpClient(handler))
+        { ;
+            using (var client = new HttpClient())
             {
                 var environment = await _ctx.Environments.ById(environmentId);
                 client.BaseAddress = new Uri(environment.ChefAutomateUrl);
@@ -589,31 +585,6 @@ namespace act.core.etl
 
                 await smtp.SendMailAsync(mm);
             }
-        }
-
-        private static bool ValidateServerCertificate(
-            object sender,
-            X509Certificate certificate,
-            X509Chain chain,
-            SslPolicyErrors sslPolicyErrors)
-        {
-            if (chain.ChainStatus.Length == 0)
-                return true;
-            var now = DateTime.UtcNow;
-            var certificateAuthority = new X509Certificate("CSG.crt");
-            var caEffectiveDate = DateTime.Parse(certificateAuthority.GetEffectiveDateString());
-            var caExpirationDate = DateTime.Parse(certificateAuthority.GetExpirationDateString());
-
-            // Check if CA certificate is valid.
-            if (now < caEffectiveDate
-                || now > caExpirationDate)
-                return false;
-
-            return chain.ChainElements
-                .Cast<X509ChainElement>()
-                .Select(element => element.Certificate)
-                .Where(chainCertificate => chainCertificate.Subject == certificateAuthority.Subject)
-                .Any(chainCertificate => chainCertificate.GetRawCertData().SequenceEqual(certificateAuthority.GetRawCertData()));
         }
     }
 }
