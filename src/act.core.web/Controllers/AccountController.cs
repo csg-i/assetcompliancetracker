6f7d384a7bac61583ed4d6f7ce304521c940f337
Roger Wolfe <roger.wolfe@csgi.com>
2025-04-28 14:07:55 -0500

    enable nullable

--- src/act.core.web/Controllers/AccountController.cs
+++ src/act.core.web/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿#nullable enable
+using System.Threading.Tasks;
 using act.core.web.Extensions;
 using act.core.web.Framework;
 using Microsoft.AspNetCore.Authentication;
