59cdb700aed81fa7838b94e9b155822d5502d114
Priyanka Kovermanne Somashekhara <Priyanka.KovermanneSomashekhara@csgi.com>
2025-04-28 11:29:08 -0400

    Modified AccountController.cs file

--- src/act.core.web/Controllers/AccountController.cs
+++ src/act.core.web/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using act.core.web.Extensions;
 using act.core.web.Framework;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.WsFederation;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -10,16 +11,15 @@ namespace act.core.web.Controllers
 {
     public class AccountController : PureMvcControllerBase
     {
-
         public AccountController(ILoggerFactory logger) : base(logger)
         {
         }
 
-        public RedirectResult SignIn(string returnUrl)
+        public IActionResult SignIn(string returnUrl)
         {
             Logger.LogDebug("Explicit sign-in occurred.");
             SetNoCacheHeader();
-            return Redirect(returnUrl ?? Url.Home());
+            return LocalRedirect(returnUrl ?? Url.Home());
         }
 
         public override SignOutResult SignOut()
@@ -29,8 +29,11 @@ namespace act.core.web.Controllers
                 Response.Cookies.Delete("ACT");
 
             SetNoCacheHeader();
-            HttpContext.SignOutAsync(WsFederationDefaults.AuthenticationScheme);
-            return new SignOutResult(WsFederationDefaults.AuthenticationScheme);
+            HttpContext.SignOutAsync(WsFederationDefaults.AuthenticationScheme).Wait();
+            return new SignOutResult(WsFederationDefaults.AuthenticationScheme, new AuthenticationProperties
+            {
+                RedirectUri = Url.Home()
+            });
         }
     }
 }
