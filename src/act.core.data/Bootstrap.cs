39d785003b9e41420c3535ac238f5fdc9063c0d7
BhaktiA <77092852+BhaktiA@users.noreply.github.com>
2025-05-20 14:01:11 +0530

    rolling back few chnages of 3.1 (#83)

--- src/act.core.data/Bootstrap.cs
+++ src/act.core.data/Bootstrap.cs
@@ -4,8 +4,6 @@ using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
-using Pomelo.EntityFrameworkCore.MySql.Extensions;
-using Pomelo.EntityFrameworkCore.MySql.Storage;
 
 namespace act.core.data
 {
@@ -15,21 +13,21 @@ namespace act.core.data
         {
             return services.AddDbContext<ActDbContext>(options =>
 
-                 options.UseMySql(configuration.GetConnectionString(connectionStringName), o =>
-                 {
-                     o.ServerVersion(new Version(5, 6, 10), ServerType.MySql);
-                     o.CommandTimeout(commandTimeout);
-                 }));
+                options.UseMySql(configuration.GetConnectionString(connectionStringName), o =>
+                    {
+                        o.ServerVersion(new Version(5, 6, 10), ServerType.MySql);
+                        o.CommandTimeout(commandTimeout);
+                    }));
 
         }
         public static IServiceCollection AddActDbContextPool(this IServiceCollection services, IConfiguration configuration, int commandTimeout=30, string connectionStringName = "ActDb")
         {
             return services.AddDbContextPool<ActDbContext>(options =>
-                               options.UseMySql(configuration.GetConnectionString(connectionStringName), o =>
-                               {
-                                   o.ServerVersion(new Version(5, 6, 10), ServerType.MySql);
-                                   o.CommandTimeout(commandTimeout);
-                               }));
+                options.UseMySql(configuration.GetConnectionString(connectionStringName), o =>
+                    {
+                        o.ServerVersion(new Version(5, 6, 10), ServerType.MySql);
+                        o.CommandTimeout(commandTimeout);
+                    }));
         }
     }
 }
